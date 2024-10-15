b1227ad4a6f1b0e792b6f51d9213d45cac827355
David Hicks <128887763+VvGJlshn5XK@users.noreply.github.com>
2024-10-15 22:51:06 +0800

    ad

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const glob = require("glob");
 const config = require("./config");
 
 const targetDir = config.targetDir;
+const ads = config.ads;
 console.log(`targetDir: ${targetDir}`);
 
 const nfoFiles = glob.sync("*.nfo", { cwd: targetDir, nodir: true });
 
 nfoFiles.forEach((nfoFile) => {
   // 新建nfoFile的前缀名称的文件夹
-  const nfoFileDir = path.dirname(nfoFile);
   const nfoFileName = nfoFile.replace(/\.nfo$/, "");
-  const newDir = path.join(targetDir, nfoFileDir, nfoFileName);
+  const newDir = path.join(targetDir, noWithAd(nfoFileName));
   fs.mkdirSync(newDir, { recursive: true });
   console.log(`mkdir ${newDir}`);
 
@@ -33,10 +33,14 @@ nfoFiles.forEach((nfoFile) => {
         newDir,
         `${keyword}${path.extname(otherFile)}`
       );
-      fs.renameSync(otherFilePath, newFilePath);
+      fs.renameSync(otherFilePath, noWithAd(newFilePath));
     } else {
       // 剪切
-      fs.renameSync(otherFilePath, newFilePath);
+      fs.renameSync(otherFilePath, noWithAd(newFilePath));
     }
   });
 });
+
+function noWithAd(str) {
+  return ads.reduce((acc, ad) => acc.replace(ad, ""), str);
+}
